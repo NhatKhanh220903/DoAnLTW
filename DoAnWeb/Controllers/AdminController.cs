7a8bba89e6c91bf35f6606f81600525b4b6455e6
Nhat Khanh <phamvonhatkhanh@gmail.com>
2024-08-09 05:59:03 +0700

    upfile

---
+++ DoAnWeb/Controllers/AdminController.cs
@@ -0,0 +1,345 @@
+﻿using DoAnWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        DataClasses1DataContext db=new DataClasses1DataContext();
+        public ActionResult IndexAdmin()
+        {
+            if (Session["user"] == null)
+            {
+                return RedirectToAction("DangNhap","Home");
+            }
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: admin/Home_admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: admin/Home_admin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: admin/Home_admin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: admin/Home_admin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: admin/Home_admin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: admin/Home_admin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult IndexKhachHang()
+        {
+            List<KHACHHANG> data =db.KHACHHANGs.ToList();
+            return View(data);
+        }
+        public ActionResult IndexThuongHieu()
+        {
+            List<THUONGHIEUXE> data = db.THUONGHIEUXEs.ToList();
+            return View(data);
+        }
+        public ActionResult IndexNhaCC()
+        {
+            List<NHACUNGCAP> data = db.NHACUNGCAPs.ToList();
+            return View(data);
+        }
+        public ActionResult danhMucXe()
+        {
+            List<XEMAY> data = db.XEMAYs.ToList();
+            return View(data);
+        }
+        public ActionResult CreateLX()
+        {
+            return View();
+        }
+
+        // POST: admin/Khachhang/Create
+        [HttpPost]
+        public ActionResult CreateLX(LOAIXE collection)
+        {
+            try
+            {
+                db.LOAIXEs.InsertOnSubmit(collection);
+                db.SubmitChanges();
+                return RedirectToAction("IndexLoaiXe");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: admin/Khachhang/Edit/5
+        public ActionResult EditLX(string id)
+        {
+
+            var customer = db.LOAIXEs.Single(x => x.MALOAIXE == id);
+            return View(customer);
+        }
+
+        [HttpPost]
+        public ActionResult EditLX(string id, LOAIXE kh)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                LOAIXE nd = db.LOAIXEs.Single(x => x.MALOAIXE == id);
+                nd.TENLOAIXE = kh.TENLOAIXE;
+                db.SubmitChanges();
+                return RedirectToAction("IndexLoaiXe");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult DeleteLX(string id)
+        {
+            var customer = db.LOAIXEs.Single(x => x.MALOAIXE == id);
+            return View(customer);
+        }
+
+        // POST: admin/Khachhang/Delete/5
+        [HttpPost]
+        public ActionResult DeleteLX(string id, LOAIXE kh)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var data = db.LOAIXEs.Single(x => x.MALOAIXE == id);
+                db.LOAIXEs.DeleteOnSubmit(data);
+                db.SubmitChanges();
+                return RedirectToAction("IndexLoaiXe");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult CreateTH()
+        {
+            return View();
+        }
+
+        // POST: admin/Khachhang/Create
+        [HttpPost]
+        public ActionResult CreateTH(THUONGHIEUXE collection)
+        {
+            try
+            {
+                db.THUONGHIEUXEs.InsertOnSubmit(collection);
+                db.SubmitChanges();
+                return RedirectToAction("IndexTHUONGHIEU");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: admin/Khachhang/Edit/5
+        public ActionResult EditTH(string id)
+        {
+
+            var customer = db.THUONGHIEUXEs.Single(x => x.MATHUONGHIEU == id);
+            return View(customer);
+        }
+
+        [HttpPost]
+        public ActionResult EditTH(string id, THUONGHIEUXE kh)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                THUONGHIEUXE nd = db.THUONGHIEUXEs.Single(x => x.MATHUONGHIEU == id);
+                nd.TENTHUONGHIEU = kh.TENTHUONGHIEU;
+                db.SubmitChanges();
+                return RedirectToAction("IndexTHUONGHIEU");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult DeleteTH(string id)
+        {
+            var customer = db.THUONGHIEUXEs.Single(x => x.MATHUONGHIEU == id);
+            return View(customer);
+        }
+
+        // POST: admin/Khachhang/Delete/5
+        [HttpPost]
+        public ActionResult DeleteTH(string id, THUONGHIEUXE kh)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var data = db.THUONGHIEUXEs.Single(x => x.MATHUONGHIEU == id);
+                db.THUONGHIEUXEs.DeleteOnSubmit(data);
+                db.SubmitChanges();
+                return RedirectToAction("IndexTHUONGHIEU");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult CreateNCC()
+        {
+            return View();
+        }
+
+        // POST: admin/Khachhang/Create
+        [HttpPost]
+        public ActionResult CreateNCC(NHACUNGCAP collection)
+        {
+            try
+            {
+                db.NHACUNGCAPs.InsertOnSubmit(collection);
+                db.SubmitChanges();
+                return RedirectToAction("IndexCungCap");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult DeleteNCC(string id)
+        {
+            var customer = db.NHACUNGCAPs.Single(x => x.MANCC == id);
+            return View(customer);
+        }
+
+        // POST: admin/Khachhang/Delete/5
+        [HttpPost]
+        public ActionResult DeleteNCC(string id, NHACUNGCAP kh)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var data = db.NHACUNGCAPs.Single(x => x.MANCC == id);
+                db.NHACUNGCAPs.DeleteOnSubmit(data);
+                db.SubmitChanges();
+                return RedirectToAction("IndexCungCap");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult EditKH(string id)
+        {
+
+            var customer = db.KHACHHANGs.Single(x => x.MAKHACHHANG == id);
+            return View(customer);
+        }
+
+        [HttpPost]
+        public ActionResult EditKH(string id, KHACHHANG kh)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                KHACHHANG nd = db.KHACHHANGs.Single(x => x.MAKHACHHANG == id);
+                nd.TENKHACHHANG = kh.TENKHACHHANG;
+                nd.NGAYSINH = kh.NGAYSINH;
+                nd.GIOITINH = kh.GIOITINH;
+                nd.SODIENTHOAI = kh.SODIENTHOAI;
+                nd.TAIKHOAN = kh.TAIKHOAN;
+                nd.MATKHAU = kh.MATKHAU;
+                nd.EMAIL = kh.EMAIL;
+                nd.DIACHI = kh.DIACHI;
+                db.SubmitChanges();
+                return RedirectToAction("IndexKhachHang");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult DeleteKH(string id)
+        {
+            var customer = db.KHACHHANGs.Single(x => x.MAKHACHHANG == id);
+            return View(customer);
+        }
+
+        // POST: admin/Khachhang/Delete/5
+        [HttpPost]
+        public ActionResult DeleteKH(string id, KHACHHANG kh)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var data = db.KHACHHANGs.Single(x => x.MAKHACHHANG == id);
+                db.KHACHHANGs.DeleteOnSubmit(data);
+                db.SubmitChanges();
+                return RedirectToAction("IndexKhachHang");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- DoAnWeb/Controllers/HomeController.cs
+++ DoAnWeb/Controllers/HomeController.cs
@@ -9,10 +9,122 @@ namespace DoAnWeb.Controllers
     public class HomeController : Controller
     {
         DataClasses1DataContext db = new DataClasses1DataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Main()
+        {
+            return View();
+        }
+        public ActionResult XeMay()
+        {
+            List<XEMAY> listProDuct = db.XEMAYs.ToList();
+            return View(listProDuct);
+
+        }
         public ActionResult XeMay8xemay()
         {
             var list = db.XEMAYs.Take(8).ToList();
             return View(list);
+        }
+        public ActionResult LoaiXe()
+        {
+            List<LOAIXE> dsLoaiXe = db.LOAIXEs.Take(8).ToList();
+            return PartialView(dsLoaiXe);
+                    
+        }
+        public ActionResult ThuongHieuXe()
+        {
+            List<THUONGHIEUXE> dsThuongHieu=db.THUONGHIEUXEs.Take(8).ToList();
+            return PartialView(dsThuongHieu);
+        }
+        public ActionResult timXeMay(string txt_Search)
+        {
+            var list = db.XEMAYs.Where(s => s.TENXEMAY.Contains(txt_Search)).ToList();
+            int sl = list.Count;
+            ViewBag.SL = sl;
+            return View(list);
+        }
+        public ActionResult XeMayTheoLoai(string MaLoai)
+        {
+            var listXe = db.XEMAYs.Where(t => t.MALOAIXE == MaLoai).ToList();
+            if (listXe.Count == 0)
+            {
+                ViewBag.TB = "Không tìm thấy loại xe cần tìm";
+            }
+            return View(listXe);
+        }
+        public ActionResult XeMayTheoHang(string MaHang)
+        {
+            var listHang = db.XEMAYs.Where(s => s.MATHUONGHIEU == MaHang).ToList();
+            if (listHang.Count == 0)
+            {
+                ViewBag.TB = "Không tìm thấy hãng xe cần tìm.";
+            }
+            return View(listHang);
+        }
+        public ActionResult XemThongTinXe(string MaXe)
+        {
+            XEMAY listxemmay = db.XEMAYs.Single(s => s.MAXEMAY == MaXe);
+            if (listxemmay == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(listxemmay);
+        }
+        [HttpGet]
+        public ActionResult DangKyTK()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult DangKyTK(KHACHHANG formCollection)
+        {
+            try
+            {
+                db.KHACHHANGs.InsertOnSubmit(formCollection);
+                db.SubmitChanges();
+                return RedirectToAction("DangNhapTK");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DangNhapTK()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhapTK(FormCollection f)
+        {
+            var tendn = f["TenDN"];
+            var matkhau = f["MatKhau"];
+            if (tendn == "admin" && matkhau == "123456")
+            {
+                Session.Add("user", "pass");
+                return RedirectToAction("IndexAdmin", "Admin");
+            }
+            else
+            {
+                KHACHHANG kh = db.KHACHHANGs.SingleOrDefault(c => c.TAIKHOAN == tendn && c.MATKHAU == matkhau);
+                if (kh != null)
+                {
+                    ViewBag.TB = "Đăng nhập thành công";
+                    Session["taikhoan"] = kh;
+                    return RedirectToAction("Main");
+                }
+                else
+                {
+                    ViewBag.TB = "Sai tên đăng nhập hoặc sai mật khấu. Vui lòng nhập lại !";
+                }
+                return View();
+            }
         }
     }
 }

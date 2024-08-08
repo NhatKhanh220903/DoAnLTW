8a1334ebee16f59351846850175be8d63081ce83
Nhat Khanh <phamvonhatkhanh@gmail.com>
2024-08-08 15:07:44 +0700

    up file

---
+++ DoAnWeb/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoAnWeb.Models;
+namespace DoAnWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        public ActionResult XeMay8xemay()
+        {
+            var list = db.XEMAYs.Take(8).ToList();
+            return View(list);
+        }
+    }
+}
